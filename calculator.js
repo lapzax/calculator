54bcbdbcb2e1d1f9491b14c664e1fb98451274b0
lapzax <agnes90128@gmail.com>
2015-03-23 22:40:59 +0800

    Initial commit

--- calculator.js
+++ calculator.js
@@ -1,86 +1,188 @@
 $(function () {
-	var curentText = '';
-	
-	function render () {
-		$('#msg').text(curentText);	
+
+	var currentText = '';
+	var temp = '';
+	var ans = '';
+	var opt = '';
+
+	function render() {
+		$('#msg').text(currentText);	
 	}
 	
+	//Numbers
 	$('#btn7').on('click', function () {
-		curentText = curentText + '7';
-		render();
+		if(currentText==''){
+			currentText = currentText + '7';
+			render();
+		}else if(currentText=='0'){
+			currentText = '7'
+		}else{
+			currentText = currentText + '7';
+			render();
+		}
 	})
 	$('#btn8').on('click', function () {
-		curentText = curentText + '8';
-		render();
+		if(currentText==''){
+			currentText = currentText + '8';
+			render();
+		}else if(currentText=='0'){
+			currentText = '8'
+		}else{
+			currentText = currentText + '8';
+			render();
+		}
 	})
 	$('#btn9').on('click', function () {
-		curentText = curentText + '9';
-		render();
+		if(currentText==''){
+			currentText = currentText + '9';
+			render();
+		}else if(currentText=='0'){
+			currentText = '9'
+		}else{
+			currentText = currentText + '9';
+			render();
+		}
 	})
-	/*
-	$('#btnDivide').on('click', function () {
-		curentText = curentText + '2';
-		render();
-	})
-	*/
+
 	$('#btn4').on('click', function () {
-		curentText = curentText + '4';
-		render();
+		if(currentText==''){
+			currentText = currentText + '4';
+			render();
+		}else if(currentText=='0'){
+			currentText = '4'
+		}else{
+			currentText = currentText + '4';
+			render();
+		}
 	})
 	$('#btn5').on('click', function () {
-		curentText = curentText + '5';
-		render();
+		if(currentText==''){
+			currentText = currentText + '5';
+			render();
+		}else if(currentText=='0'){
+			currentText = '5'
+		}else{
+			currentText = currentText + '5';
+			render();
+		}
 	})
 	$('#btn6').on('click', function () {
-		curentText = curentText + '6';
-		render();
+		if(currentText==''){
+			currentText = currentText + '6';
+			render();
+		}else if(currentText=='0'){
+			currentText = '6'
+		}else{
+			currentText = currentText + '6';
+			render();
+		}
 	})
-	/*
-	$('#btnMultiply').on('click', function () {
-		curentText = curentText + '2';
-		render();
-	})
-	*/
+
 	$('#btn1').on('click', function () {
-		//alert('hello jquery');
-		curentText = curentText + '1';
-		render();
-		//$('#msg').addClass('green');
+		if(currentText==''){
+			currentText = currentText + '1';
+			render();
+		}else if(currentText=='0'){
+			currentText = '1'
+		}else{
+			currentText = currentText + '1';
+			render();
+		}
 	})
 	$('#btn2').on('click', function () {
-		curentText = curentText + '2';
-		render();
+		if(currentText==''){
+			currentText = currentText + '2';
+			render();
+		}else if(currentText=='0'){
+			currentText = '2'
+		}else{
+			currentText = currentText + '2';
+			render();
+		}
 	})
 	$('#btn3').on('click', function () {
-		curentText = curentText + '3';
-		render();
+		if(currentText==''){
+			currentText = currentText + '3';
+			render();
+		}else if(currentText=='0'){
+			currentText = '3'
+		}else{
+			currentText = currentText + '3';
+			render();
+		}
 	})
-	
-	$('#btnMinus').on('click', function () {
-		curentText = '-' + curentText + '2';
-		render();
-	})
-	
-	$('#btnC').on('click', function () {
-		curentText = '';
-		render();
-	})
-	$('#btn0').on('click', function () {
-		curentText = curentText + '0';
-		render();
-	})
-	/*
-	$('#btnEqual').on('click', function () {
-		curentText = curentText + '2';
-		render();
-	})
-	*/
-	/*
-	$('#btnAdd').on('click', function () {
-		curentText = curentText + '2';
-		render();
-	})
-	*/
 
+	$('#btn0').on('click', function () {
+		currentText = currentText + '0';
+		render();
+	})
 	
-});
+	//Operators
+	$('#btnDivide').on('click', function () {
+		if(currentText==''){
+			render();
+		}else if(currentText!=''){
+			temp = temp + currentText;
+			currentText = '';
+			opt = 'Divide';
+		}	
+	})
+	$('#btnMultiply').on('click', function () {
+		if(currentText==''){
+			render();
+		}else if(currentText!=''){
+			temp = temp + currentText;
+			currentText = '';
+			opt = 'Mutiply';
+		}	
+	})
+	$('#btnMinus').on('click', function () {
+		if(currentText==''){
+			currentText = '-' + currentText;
+			render();
+		}else if(currentText!=''){
+			temp = temp + currentText;
+			currentText = '';
+			opt = 'Minus';
+		}		
+	})
+	$('#btnAdd').on('click', function () {
+		if(currentText==''){
+			render();
+		}else if(currentText!=''){
+			temp = temp + currentText;
+			currentText = '';
+			opt = 'Add';
+		}		
+	})
+
+	$('#btnC').on('click', function () {
+		currentText = '0';
+		render();
+	})
+
+	$('#btnEqual').on('click', function () {
+		switch(opt){
+			case 'Divide':
+				ans = ans + (temp/currentText);
+				opt = '';
+				break;
+			case 'Mutiply':
+				ans = ans + (temp*currentText);
+				opt = '';
+				break;
+			case 'Minus':
+				ans = ans + temp - currentText;
+				opt = '';
+				break;
+			case 'Add':
+				ans = ans + temp + currentText;
+				opt = '';
+				break;
+		}
+		$('#msg').text(ans);
+	})
+	
+	
+	
+})
