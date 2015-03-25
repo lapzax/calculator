55126b4f5059e301dedf05808a2ab6048f6996e7
lapzax <agnes90128@gmail.com>
2015-03-25 12:06:33 +0800

    Initial comment

--- calculator.js
+++ calculator.js
@@ -11,109 +11,46 @@ $(function () {
 	
 	//Numbers
 	$('#btn7').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '7';
-			render();
-		}else if(currentText=='0'){
-			currentText = '7'
-		}else{
-			currentText = currentText + '7';
-			render();
-		}
+		currentText += '7';
+		render();
 	})
 	$('#btn8').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '8';
-			render();
-		}else if(currentText=='0'){
-			currentText = '8'
-		}else{
-			currentText = currentText + '8';
-			render();
-		}
+		currentText += '8';
+		render();
 	})
 	$('#btn9').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '9';
-			render();
-		}else if(currentText=='0'){
-			currentText = '9'
-		}else{
-			currentText = currentText + '9';
-			render();
-		}
+		currentText += '9';
+		render();
 	})
 
 	$('#btn4').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '4';
-			render();
-		}else if(currentText=='0'){
-			currentText = '4'
-		}else{
-			currentText = currentText + '4';
-			render();
-		}
+		currentText += '4';
+		render();
 	})
 	$('#btn5').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '5';
-			render();
-		}else if(currentText=='0'){
-			currentText = '5'
-		}else{
-			currentText = currentText + '5';
-			render();
-		}
+		currentText += '5';
+		render();
 	})
 	$('#btn6').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '6';
-			render();
-		}else if(currentText=='0'){
-			currentText = '6'
-		}else{
-			currentText = currentText + '6';
-			render();
-		}
+		currentText += '6';
+		render();
 	})
 
 	$('#btn1').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '1';
-			render();
-		}else if(currentText=='0'){
-			currentText = '1'
-		}else{
-			currentText = currentText + '1';
-			render();
-		}
+		currentText += '1';
+		render();
 	})
 	$('#btn2').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '2';
-			render();
-		}else if(currentText=='0'){
-			currentText = '2'
-		}else{
-			currentText = currentText + '2';
-			render();
-		}
+		currentText += '2';
+		render();
 	})
 	$('#btn3').on('click', function () {
-		if(currentText==''){
-			currentText = currentText + '3';
-			render();
-		}else if(currentText=='0'){
-			currentText = '3'
-		}else{
-			currentText = currentText + '3';
-			render();
-		}
+		currentText += '3';
+		render();
 	})
 
 	$('#btn0').on('click', function () {
-		currentText = currentText + '0';
+		currentText += '0';
 		render();
 	})
 	
@@ -122,7 +59,7 @@ $(function () {
 		if(currentText==''){
 			render();
 		}else if(currentText!=''){
-			temp = temp + currentText;
+			temp += currentText;
 			currentText = '';
 			opt = 'Divide';
 		}	
@@ -131,7 +68,7 @@ $(function () {
 		if(currentText==''){
 			render();
 		}else if(currentText!=''){
-			temp = temp + currentText;
+			temp += currentText;
 			currentText = '';
 			opt = 'Mutiply';
 		}	
@@ -141,7 +78,7 @@ $(function () {
 			currentText = '-' + currentText;
 			render();
 		}else if(currentText!=''){
-			temp = temp + currentText;
+			temp += currentText;
 			currentText = '';
 			opt = 'Minus';
 		}		
@@ -150,7 +87,7 @@ $(function () {
 		if(currentText==''){
 			render();
 		}else if(currentText!=''){
-			temp = temp + currentText;
+			temp += currentText;
 			currentText = '';
 			opt = 'Add';
 		}		
@@ -158,25 +95,45 @@ $(function () {
 
 	$('#btnC').on('click', function () {
 		currentText = '0';
+		temp = '';
+		ans = '';
+		opt = '';
 		render();
+		currentText ='';
 	})
 
 	$('#btnEqual').on('click', function () {
 		switch(opt){
 			case 'Divide':
-				ans = ans + (temp/currentText);
+				if(ans!=''){
+					ans /= currentText;
+				}else{
+					ans += (temp/currentText);
+				}
 				opt = '';
 				break;
 			case 'Mutiply':
-				ans = ans + (temp*currentText);
+				if(ans!=''){
+					ans *= currentText;
+				}else{
+					ans += (temp*currentText);
+				}
 				opt = '';
 				break;
 			case 'Minus':
-				ans = ans + temp - currentText;
+				if(ans!=''){
+					ans -= currentText;
+				}else{
+					ans += (temp-currentText);
+				}				
 				opt = '';
 				break;
 			case 'Add':
-				ans = ans + temp + currentText;
+				if(ans!=''){
+					ans += currentText;
+				}else{
+					ans += (temp+currentText);
+				}
 				opt = '';
 				break;
 		}
